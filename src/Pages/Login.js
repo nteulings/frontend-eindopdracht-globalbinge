a8086d62c0d7bb386e05def72447416e24ee04a9
nteulings <nteulings@yahoo.com>
2021-06-28 13:24:13 +0200

    convert login to react hook form

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -1,32 +1,66 @@
 import React from 'react';
-import {useState} from "react";
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import "./Forms.css"
 
-function Login() {
-    const [emailAdress, setEmailAdress] = useState('');
-    const [password, setPassword] = useState('');
+// [x] change to hook-form
+//    [x]  change fields form
+//    [x]  basic css styling
+// [x] install react hook form
+// [x] setup useform
+// [ ] set errors and validation
+// [ ] set error messages
+
+function SignIn() {
+   const { handleSubmit, register, formState: { errors }} = useForm();
+
+   function onSubmit(data) {
+       console.log(data)
+   }
+    console.log("wat is errors", errors)
 
     return (
-        <form>
-            <h1>Login</h1>
-            <label><h1>Email adress</h1></label>
+        <>
+        <form className={"form-container"}
+            onSubmit={handleSubmit(onSubmit)}>
+
+            <h1>LOGIN</h1>
+            <label htmlFor={"username-field"}>
+                <h3>Your username:</h3>
+            </label>
             <input
-                value = {emailAdress}
-                placeholder={'your email adress'}
-                onChange={(event => setEmailAdress(event.target.value))}
-            />
-            <label><h1>Password</h1></label>
+                type="text"
+                id={"username-field"}
+                placeholder="username"
+                name={"username"}
+                {...register("username",
+                    {required: true, maxLength: 8})}
+                />
+            <span>
+                {/*{error-message}*/}
+            </span>
+            <label htmlFor={"password-field"}>
+                <h3>Fill in your password:</h3>
+            </label>
             <input
-                value = {password}
-                placeholder={'your password'}
-                onChange={(event => setPassword(event.target.value))}
-            />
+                type="text"
+                id={"password"}
+                placeholder="password"
+                name={"password"}
+                {...register("password",
+                    {required: true, min: 6, maxLength: 8})} />
+            <span>
+                {/*{message}*/}
+            </span>
             <button
-            onClick={console.log(emailAdress,password)}
-            >submit</button>
-            <button
-            >register</button>
+                className={"form-button"}
+                onClick={console.log()}
+            >sign in</button>
+            <p>don't have an account yet? register <Link to="/register">here</Link>
+            </p>
         </form>
+        </>
     );
 }
 
-export default Login;
+export default SignIn;
