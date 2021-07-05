e1c1218fd0de0c89aadc426a753d4aae02c47608
nteulings <nteulings@yahoo.com>
2021-07-05 12:09:02 +0200

    change navigation for user when logged in or logged out

--- src/components/TopMenu.js
+++ src/components/TopMenu.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './TopMenu.css'
 import { NavLink } from 'react-router-dom';
 import Logo from '../assets/globalbing-logo.png';
 import { ImMenu } from "react-icons/im";
+import { authContext} from "../context/AuthContext";
+
+// - [x] add useContext for userdata
+// - [x] get user data from context
+// - [x] change navigation menu when logged in and logged out
 
 function TopMenu() {
+    const { logout, authState: { user } } = useContext(authContext);
+
     return (
         <nav>
             <div className="nav-container">
@@ -17,17 +24,31 @@ function TopMenu() {
                         <li>
                             <NavLink to="/about" activeClassName="active-link">About</NavLink>
                         </li>
-
+                        { user &&
                         <li>
                             <NavLink to="/countries" activeClassName="active-link">Countries</NavLink>
                         </li>
+                        }
+                        {!user ?
+                            <>
+                            <li>
+                                <NavLink to="/login" activeClassName="active-link">Login</NavLink>
+                            </li>
 
-                        <li>
-                            <NavLink to="/login" activeClassName="active-link">Login</NavLink>
-                        </li>
-                        <li>
+                            <li>
+                                <NavLink to="/register" activeClassName="active-link">Register</NavLink>
+                            </li>
+                            </>
+                            :
+                            <>
+                            <li>
                             <NavLink to="/profile" activeClassName="active-link">Profile</NavLink>
-                        </li>
+                            </li>
+                            <li>
+                            <NavLink to="/login" activeClassname={"active-link"} onClick={logout} >Logout</NavLink>
+                            </li>
+                            </>
+                        }
                     </ul>
                     <ImMenu id={'hamburger-menu'} size={40} />
             </div>
