47bc256ba387019c5aa755d47c139010727e66f6
nteulings <nteulings@yahoo.com>
2021-07-07 10:25:35 +0200

    Fetch and check countrydata

--- src/Pages/Countries.js
+++ src/Pages/Countries.js
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import axios from "axios";
+const apiKey = `${process.env.REACT_APP_API_KEY}`
+
+
+// fetch country data
+// - [x] set ApiKey in .env and make variable apiKey
+// - [x] import useeffect
+// - [x] set a useeffect to trigger onMount
+// - [x] write a asynch function
+// - [x] call it in useeffect
+// - [x] check data
 
 
 
 function Countries() {
-    const options = {
-        method: 'GET',
-        url: 'https://unogsng.p.rapidapi.com/countries',
-        headers: {
-            'x-rapidapi-key': '2e8b09e3d2msh1084c477e7f77c9p1d3a48jsn5f4876882df6',
-            'x-rapidapi-host': 'unogsng.p.rapidapi.com'
-        }
-    };
 
-    axios.request(options).then(function (response) {
-        console.log(response.data);
-    }).catch(function (error) {
-        console.error(error);
-    });
+useEffect(() => {
+    async function fetchCountryData() {
+        console.log("on mount");
+            const response = await axios.get('https://unogsng.p.rapidapi.com/countries'
+                , {
+                    headers: {
+                        'x-rapidapi-key': apiKey,
+                        'x-rapidapi-host': 'unogsng.p.rapidapi.com'
+                    }
+                });
+            console.log('what is response', response.data.results);
+        }
+        fetchCountryData();
+    }, [ ]);
+
+
     return (
-        <div className="page-container">
-            <h1>Countries</h1>
-        </div>
+    <div>
+    <h1>Countries</h1>
+    </div>
     );
 }
 
-export default Countries;
+export default Countries
