01c977f9099d534b98819a3a7daf86564839351b
nteulings <nteulings@yahoo.com>
2021-07-16 09:15:32 +0200

    Use params and get data from MovieCard.js

--- src/components/ExpMovies.js
+++ src/components/ExpMovies.js
@@ -1,14 +1,19 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from "axios";
+import { useParams } from "react-router-dom";
+import MovieCard from "./MovieCard";
+// import {countryDataContext} from "../Pages/Countries";
 const apiKey = `${process.env.REACT_APP_API_KEY}`
 
-// - [x] fetch expiring content per country
-
-
+// The function ExpMovies fetches a list of expiring movies per country.
+// The result doesn't contain any detailinfo (poster image, title
 
 function ExpMovies() {
+    const { id, name } = useParams()
+    // const countryID = useContext(countryDataContext);
+    // console.log("whats countryID", countryID);
     const [expMoviesData, setExpMoviesData] = useState(null);
-    console.log("expMoviesData", expMoviesData)
+    console.log("expMoviesData", expMoviesData, id, name)
     // const expTitleIds = expMoviesData.map((id) => {return id.netflixid});
     // console.log("what is expTitleIds", expTitleIds)
     // const today = new Date();
@@ -16,17 +21,21 @@ function ExpMovies() {
 
     useEffect(() => {
         async function fetchExpMovies() {
-            console.log("on mount");
+            console.log("id", id);
+            console.log("name", name)
             // console.log("what is expmoviesdata", expMoviesData)
-            const response = await axios.get('https://unogsng.p.rapidapi.com/expiring'
-                , {
-                    params: {countrylist: '67'},
+            const response = await axios.get('https://unogsng.p.rapidapi.com/expiring', {
+                    params: {
+                        countrylist: id,
+                        limit: 5,
+                    },
                     headers: {
                         'x-rapidapi-key': apiKey,
                         'x-rapidapi-host': 'unogsng.p.rapidapi.com'
                     }
                 });
             setExpMoviesData(response.data.results);
+            console.log(response.data.results);
             // console.log("expMoviesData", expMoviesData);
             // gives an object with array of results {netflixid:}
             // gives an object with array of results {nfid:}
@@ -39,8 +48,20 @@ function ExpMovies() {
     }, []);
 
     return (
-        <h1>expiring movies</h1>
+        <>
+            <h2>Expiring Netflix content in {name}</h2>
+                <ul className={"poster-images" }>
+
+            {/*2. Voor iedere film, geef een movie-component weer. Dit component krijgt de properties netflixId mee*/}
+
+                    {expMoviesData && expMoviesData.map((expMoviesData) => {
+                        return (
+                            <MovieCard netflixId={expMoviesData.netflixid} />
+                )})
+            }
+            </ul>
+        </>
     )
-}
+};
 
 export default ExpMovies;
