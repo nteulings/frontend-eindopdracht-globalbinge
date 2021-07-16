d34d0143a18c958b760645f8113af5c70be1e78a
nteulings <nteulings@yahoo.com>
2021-07-16 17:36:22 +0200

    cleaned

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,14 +2,12 @@ import React, {createContext, useEffect, useState} from "react";
 import axios from "axios";
 import {useHistory} from "react-router-dom";
 
-// - [x] get token from local storage
-// - [x] check data on refresh
-// - [x] remove token with logout function
-// - [x] set authstate user to null
-// - [x] disabled deps error eslint
+// AuthContext
+// This provider manage authorization to the application.
+// after successful login token is stored in localstorage.
+// and is still available after refresh
 
 export const authContext = createContext({});
-
 function AuthContextProvider({ children }) {
 
     const [authState, setAuthState] = useState({user: null, status: 'pending'});
@@ -17,7 +15,6 @@ function AuthContextProvider({ children }) {
 
     useEffect(() => {
         const token = localStorage.getItem("token");
-        // // no token? back to login
         if (token) {
             login(token);
         } else {
@@ -31,24 +28,17 @@ function AuthContextProvider({ children }) {
     }, []);
 
     function login(token, id) {
-        console.log("do we have a token and id", token, id);
         localStorage.setItem('token', token);
         fetchUserData(token, id);
-        // check if necessary for storage? const dataFromToken = jwt_decode(token);
-        // const userId = id
-        // console.log("what is", id);
     }
-
     async function fetchUserData(token, id) {
         try {
             const response = await axios.get(`https://polar-lake-14365.herokuapp.com/api/user`, {
                 headers: {
                     Authorization: `Bearer ${token}`,
-
                 }
             });
             console.log(response);
-            // console.log(response.data.username);
             setAuthState({ user: response.data, status: "done" });
 
             history.push('/loginSuccess');
@@ -56,7 +46,6 @@ function AuthContextProvider({ children }) {
             console.error(e);
         }
     }
-
 
     function logout() {
     localStorage.removeItem('token');
@@ -76,22 +65,3 @@ function AuthContextProvider({ children }) {
 }
 
 export default AuthContextProvider;
-
-// #Context:
-// [x] create AuthContext.js
-// [x] make AuthContext with createContext
-// 2. [x] build AuthContextProvider:
-//    - [x] children
-//    - [x] AuthContext.Provider component
-//    - [x] empty data object
-// 3. [x] pass data object value={} property to .Provider
-// 4. [x] export context and provider component
-// 5. [x] import provider component in index.js
-
-//#Setup context:
-// - [x] Which data in context?
-// - [x] functions login logout
-// - [x] state for user and status(user => null en status => 'pending')
-// - [x] use effect for refresh (mountig cycle)
-// - [x] status done? children visable
-// - [x] set state en functions in data object
