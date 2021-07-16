319b6fd7c6475faacd9253fd1eb4c0431f9a2ead
nteulings <nteulings@yahoo.com>
2021-07-16 09:08:25 +0200

    changes in route with params

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 import TopMenu from '../src/components/TopMenu'
 import About from "./Pages/About";
 import Countries from './Pages/Countries';
-import Login from './Pages/Login';
+import Login from './Pages/Forms/Login';
 import HomePage from "./Pages/Home";
-import Register from "./Pages/Register";
+import Register from "./Pages/Forms/Register";
 import Profile from "./Pages/Profile";
 import Search from "./Pages/Search";
 import DetailInfo from "./Pages/DetailInfo";
-import LoginSuccess from "./Pages/LoginSuccess";
-import RegisterSuccess from "./Pages/RegisterSuccess"
-
+import LoginSuccess from "./Pages/SuccessPages/LoginSuccess";
+import RegisterSuccess from "./Pages/SuccessPages/RegisterSuccess"
+import ExpMovies from "./components/ExpMovies";
+import SearchDetails from "./components/SearchDetails";
 import {
     Switch,
     Route,
@@ -21,7 +22,7 @@ function App() {
   return (
       <>
           <TopMenu />
-          <Switch>
+            <Switch>
               <Route exact path="/">
                   <HomePage />
               </Route>
@@ -31,28 +32,34 @@ function App() {
               <Route path="/loginsuccess">
                   <LoginSuccess />
               </Route>
-              <Route path="/detailinfo">
-                  <DetailInfo />
-              </Route>
               <Route path="/about">
                   <About />
               </Route>
-              <Route path="/countries">
+              <Route exact path="/countries">
                   <Countries />
               </Route>
-              <Route path="/search">
+              <Route exact path="/countries/:id/:name">
+                  <ExpMovies />
+              </Route>
+              <Route exact path="/search">
                   <Search />
               </Route>
-              <Route path="/login">
+                <Route exact path="/search/:nfid/:clist">
+                    <SearchDetails />
+                </Route>
+              <Route path="/detailinfo">
+                  <DetailInfo />
+              </Route>
+              <Route exact path="/login">
                   <Login />
               </Route>
-              <Route path="/register">
+              <Route exact path="/register">
                   <Register />
               </Route>
               <Route path="/profile">
                   <Profile />
               </Route>
-          </Switch>
+            </Switch>
       </>
   );
 }
