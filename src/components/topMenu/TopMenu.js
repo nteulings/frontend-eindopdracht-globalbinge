acc969d3bf41ab2ee3a32449f15d2c94ff6adc14
nteulings <nteulings@yahoo.com>
2021-07-18 17:16:59 +0200

    adjust stylesheet

---
+++ src/components/topMenu/TopMenu.js
@@ -0,0 +1,77 @@
+import React, { useContext } from 'react';
+import './TopMenu.css'
+import { NavLink } from 'react-router-dom';
+import Logo from '../../assets/globalbing-logo.png';
+import {ImInfo, ImMenu, ImUser, ImUserCheck, ImSearch, ImEarth} from "react-icons/im";
+
+
+
+import { authContext} from "../../context/AuthContext";
+
+// - [x] add useContext for userdata
+// - [x] get user data from context
+// - [x] change navigation menu when logged in and logged out
+// - [x] make search available
+// - [x] delete home and add navigation to /home to logo.
+
+function TopMenu() {
+    const { logout, authState: { user } } = useContext(authContext);
+
+    return (
+        <div className="header">
+        <nav>
+            <div className="nav-container">
+                <NavLink to="/" exact><img src={Logo} alt="Logo" id={"logo"}/></NavLink>
+                    <ul className={"navigation"}>
+                        <li>
+                            <NavLink to="/about" className="screen"
+                                     activeClassName="active-link">About</NavLink>
+                            <NavLink to="/about" className="mobile"><ImInfo /></NavLink>
+                        </li>
+                    { user &&
+                    <>
+                        <li>
+                            <NavLink to="/countries"
+                                         activeClassName="active-link"
+                                         className="screen"
+                                >Countries
+                                </NavLink>
+                            <NavLink to="/countries" className="mobile"><ImEarth /></NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/search" className="screen"
+                                     activeClassName="active-link">Search</NavLink>
+                            <NavLink to="/search" className="mobile"><ImSearch /></NavLink>
+                        </li>
+                    </>
+                    }
+                    {!user ?
+                    <>
+                        <li>
+                            <NavLink to="/login" activeClassName="active-link">Login</NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/register" activeClassName="active-link">Register</NavLink>
+                        </li>
+                        </>
+                        :
+                    <>
+                        <li>
+                            <NavLink to="/login"
+                                     activeClassname="active-link"
+                                     onClick={logout} >Logout
+                            </NavLink>
+                        </li>
+                        <li>
+                            <NavLink to="/profile"><ImUser /></NavLink>
+                        </li>
+                    </>
+                    }
+                    </ul>
+            </div>
+        </nav>
+        </div>
+    );
+}
+
+export default TopMenu;
